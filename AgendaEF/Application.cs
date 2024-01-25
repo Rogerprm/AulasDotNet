8e7775d2c50caba1b318508ad785809a8972f194
roger <rogerprimiani@gmail.com>
2024-01-25 17:11:27 -0300

    AgendaEF

---
+++ AgendaEF/Application.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaEF
+{
+    public class Application
+    {
+    }
+}
---
+++ AgendaEF/Models/Tarefa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaEF.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; }
+        public string Descricao { get; set; }
+        public string Responsavel { get; set; }
+        public int ReuniaoId { get; set; }
+        public Reuniao Reuniao { get; set; }
+    }
+}
